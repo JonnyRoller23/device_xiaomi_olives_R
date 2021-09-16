b0000413bcba28c51f0270e7cedddf13a781b429
IAmDeadlylxrd <IAmDeadlylxrd@gmail.com>
2021-09-16 15:45:13 +0500

    drop support for olive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,16 +10,13 @@ include device/xiaomi/sdm439-common/BoardConfigCommon.mk
 DEVICE_PATH := device/xiaomi/olives
 
 # Asserts
-TARGET_OTA_ASSERT_DEVICE := olive,olivelite,olivewood,olives
+TARGET_OTA_ASSERT_DEVICE := olivelite,olivewood
 
 # Build broken rules
 BUILD_BROKEN_DUP_RULES := true
 
 # Enable real time lockscreen charging current values
 BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
-
-# HIDL
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_olives
@@ -34,10 +31,6 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 4294967296
 
 # Security patch level
 VENDOR_SECURITY_PATCH := 2021-07-01
-
-# SELinux
-BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/biometrics/sepolicy
-BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Inherit from the proprietary version
 include vendor/xiaomi/olives/BoardConfigVendor.mk
