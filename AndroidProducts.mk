ad0e7f66b2d9c1866d497cd70228b3af4707acae
JonnyRoller23 <maxim55miui11@gmail.com>
2022-01-16 00:24:17 +0300

    olives: Btingup to LineageOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The havocOS Project
+# Copyright (C) 2021 The lineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_olives.mk
+    $(LOCAL_DIR)/lineage_olives.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_olives-user \
-    havoc_olives-userdebug \
-    havoc_olives-eng
+    lineage_olives-user \
+    lineage_olives-userdebug \
+    lineage_olives-eng
--- lineage_olives.mk
+++ lineage_olives.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The havocOS Project
+# Copyright (C) 2021 The lineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,15 +9,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common havocOS stuff.
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+# Inherit some common lineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from olives device
 $(call inherit-product, device/xiaomi/olives/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := olives
-PRODUCT_NAME := havoc_olives
+PRODUCT_NAME := lineage_olives
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 8A
@@ -26,15 +26,14 @@ TARGET_VENDOR := Xiaomi
 
 # Some build flags
 TARGET_BOOT_ANIMATION_RES := 720
-TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_FACE_UNLOCK := true
 TARGET_USES_BLUR := true
-TARGET_INCLUDE_PIXEL_CHARGER := true
-IS_PHONE := true
+TARGET_USES_AOSP_CHARGER := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_INCLUDE_STOCK_ARCORE := false
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="olive-user 10 QKQ1.191014.001 V12.5.1.0.QCNMIXM release-keys"
-
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Xiaomi/olive/olive:10/QKQ1.191014.001/V12.5.1.0.QCNMIXM:user/release-keys"
+PRODUCT_NAME=olives
